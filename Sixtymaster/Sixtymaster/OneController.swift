e35bf6f05bdeb511ccf746eafce9b8f47201b421
jheng.wei0919@gmail.com <JackPan@jackpan-MacBook-Pro.local>
2019-02-05 21:13:17 +0800

    add actionsheet

--- Sixtymaster/Sixtymaster/OneController.swift
+++ Sixtymaster/Sixtymaster/OneController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import GoogleMobileAds
 
-class OneController: UIViewController,GADBannerViewDelegate {
+class OneController: UIViewController,GADBannerViewDelegate , UITabBarDelegate,UIActionSheetDelegate{
     var adBannerView: GADBannerView?
 
     override func viewDidLoad() {
@@ -18,6 +18,11 @@ class OneController: UIViewController,GADBannerViewDelegate {
         // Do any additional setup after
         setAdBanner()
     }
+    @IBOutlet weak var tabbar_item_1: UITabBarItem!
+    @IBOutlet weak var tabbar_item_2: UITabBarItem!
+    @IBOutlet weak var tabbar_item_3: UITabBarItem!
+    @IBOutlet weak var tabbar_item_4: UITabBarItem!
+    var alert : UIActionSheet?
     func setAdBanner(){
         let id = "ca-app-pub-7019441527375550/3838969590"
         adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
@@ -92,4 +97,34 @@ class OneController: UIViewController,GADBannerViewDelegate {
     }
     */
 
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+        let tabIndex = tabBar.items?.index(of: item)
+
+        if tabIndex == 0 {
+            setActionSheep()
+        }else if tabIndex == 1 {
+            print("1")
+
+        }else if tabIndex == 2 {
+            print("2")
+
+            
+        }else if tabIndex == 3 {
+            print("3")
+
+            
+        }
+    }
+    func setActionSheep(){
+       
+        alert =  UIActionSheet(title: "解籤", delegate: self, cancelButtonTitle: "取消", destructiveButtonTitle: nil, otherButtonTitles: "4","9","3")
+        alert?.show(in: self.view)
+        
+    }
+    func actionSheet(_ actionSheet: UIActionSheet, clickedButtonAt buttonIndex: Int) {
+        print(buttonIndex)
+    }
+   
+    
 }
