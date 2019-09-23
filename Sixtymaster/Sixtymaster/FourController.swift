37858bf97b080329a208dbcd54197132663217b4
Wei Wei <jheng.wei0919@gmail.com>
2019-09-23 15:55:27 +0800

    add code

--- Sixtymaster/Sixtymaster/FourController.swift
+++ Sixtymaster/Sixtymaster/FourController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class FourController: UIViewController {
-
+class FourController: UIViewController ,GADBannerViewDelegate  {
+    var adBannerView: GADBannerView?
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAdBanner()
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +28,67 @@ class FourController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func setAdBanner(){
+        let id = "ca-app-pub-7019441527375550/3838969590"
+        adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        adBannerView!.adUnitID = id
+        adBannerView!.delegate = self
+        adBannerView!.rootViewController = self
+        
+        adBannerView!.load(GADRequest())
+    }
+    
+    // Called when an ad request loaded an ad.
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        addBannerViewToView(bannerView)
+        
+        print(bannerView.adUnitID)
+    }
+    
+    // Called when an ad request failed.
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        print((error.localizedDescription))
+    }
+    
+    // Called just before presenting the user a full screen view, such as a browser, in response to
+    // clicking on an ad.
+    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    
+    // Called just before dismissing a full screen view.
+    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    
+    // Called just after dismissing a full screen view.
+    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    
+    // Called just before the application will background or terminate because the user clicked on an
+    // ad that will launch another application (such as the App Store).
+    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: bottomLayoutGuide,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+            ])
+    }
 }
