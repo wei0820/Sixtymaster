118edb2b98df33cbefd7a946cb425ff93e8502ec
Wei Wei <jheng.wei0919@gmail.com>
2019-09-24 17:05:56 +0800

    add class function

---
+++ Sixtymaster/Sixtymaster/ItemClass.swift
@@ -0,0 +1,18 @@
+//
+//  ItemClass.swift
+//  Sixtymaster
+//
+//  Created by oneplay on 2019/9/24.
+//  Copyright © 2019 jackpan. All rights reserved.
+//
+
+import Foundation
+
+class itemClass {
+    let mImageId = 0
+    let mMessageItem = ""
+    
+    static func setImageView(){
+        
+    }
+}
