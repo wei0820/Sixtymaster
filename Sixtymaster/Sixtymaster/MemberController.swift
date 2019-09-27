6c202cbcc0e4272674825245e8a0e4837680f826
Wei Wei <jheng.wei0919@gmail.com>
2019-09-27 17:19:09 +0800

    add google login

--- Sixtymaster/Sixtymaster/MemberController.swift
+++ Sixtymaster/Sixtymaster/MemberController.swift
@@ -11,24 +11,31 @@ import Firebase
 import GoogleSignIn
 class MemberController: UIViewController ,GIDSignInDelegate   {
     
+    @IBOutlet weak var mIdLabel: UILabel!
+    @IBOutlet weak var mMaliLabel: UILabel!
+    @IBOutlet weak var mImg: UIImageView!
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!){
         if let err = error {
             print ("failed to log into Google", err)
             return
         }
-
-        print("successfully logged into Google",user)
-        self.toggleAuthUI()
-
-
-
+        
+        guard let authentication = user.authentication else {return}
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            self.toggleAuthUI()
+            
         }
-    
+    }
     @IBOutlet weak var signOutButton: UIButton!
     @IBAction func userLogOut(_ sender: Any) {
         GIDSignIn.sharedInstance().signOut()
-           // [START_EXCLUDE silent]
-           toggleAuthUI()
+        // [START_EXCLUDE silent]
+        toggleAuthUI()
     }
     
     override func viewDidLoad() {
@@ -43,15 +50,15 @@ class MemberController: UIViewController ,GIDSignInDelegate   {
         // Do any additional setup after loading the view.'
         // Google Sing in Login
         
-
+        
         // Automatically sign in the user.
         GIDSignIn.sharedInstance()?.presentingViewController = self
-
+        
         GIDSignIn.sharedInstance()?.restorePreviousSignIn()
         GIDSignIn.sharedInstance().delegate = self
-
-
-
+        
+        
+        
         toggleAuthUI()
         
     }
@@ -63,16 +70,33 @@ class MemberController: UIViewController ,GIDSignInDelegate   {
             // Signed in
             signInButton.isHidden = true
             signOutButton.isHidden = false
-
+            
             print(GIDSignIn.sharedInstance()?.currentUser?.userID)
             print(GIDSignIn.sharedInstance()?.currentUser?.profile.email)
             print(GIDSignIn.sharedInstance()?.currentUser?.profile.imageURL(withDimension: 400))
+            let id = GIDSignIn.sharedInstance()?.currentUser?.userID
+            let mail = GIDSignIn.sharedInstance()?.currentUser?.profile.email
+            mIdLabel.text = id
+            mMaliLabel.text = mail
             
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                let imageData:NSData = NSData(contentsOf: (GIDSignIn.sharedInstance()?.currentUser?.profile.imageURL(withDimension: 400))!)!
+
+                // When from background thread, UI needs to be updated on main_queue
+               DispatchQueue.main.async {
+                    let image = UIImage(data: imageData as Data)
+                self.mImg.image = image
+                }
+            }
+
         } else {
             signInButton.isHidden = false
             signOutButton.isHidden = true
+            mIdLabel.text = ""
+                 mMaliLabel.text = ""
+            mImg.image = nil
             
-
         }
     }
     
