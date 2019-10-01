07df5d66f6e7dcf48d05ea2baf6aff0d893e2d41
Wei Wei <jheng.wei0919@gmail.com>
2019-10-01 10:46:27 +0800

    add function

--- Sixtymaster/Sixtymaster/LoginViewController.swift
+++ Sixtymaster/Sixtymaster/LoginViewController.swift
@@ -7,11 +7,41 @@
 //
 
 import UIKit
-
-class LoginViewController: UIViewController {
-
+import Firebase
+import GoogleSignIn
+class LoginViewController: UIViewController ,GIDSignInDelegate{
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!){
+           if let err = error {
+               print ("failed to log into Google", err)
+               return
+           }
+           
+           guard let authentication = user.authentication else {return}
+           let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+           Auth.auth().signIn(with: credential) { (user, error) in
+               if let error = error {
+                   print(error)
+                   return
+               }
+               self.toggleAuthUI()
+               
+           }
+       }
     override func viewDidLoad() {
         super.viewDidLoad()
+        if #available(iOS 13.0, *) {
+                overrideUserInterfaceStyle = .light
+            } else {
+                // Fallback on earlier versions
+                overrideUserInterfaceStyle = .light
+
+            }
+        
+        // Automatically sign in the user.
+              GIDSignIn.sharedInstance()?.presentingViewController = self
+              
+              GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+              GIDSignIn.sharedInstance().delegate = self
 
         // Do any additional setup after loading the view.
     }
