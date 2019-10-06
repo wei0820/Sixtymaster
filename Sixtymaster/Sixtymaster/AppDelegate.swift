fd44eb6aed325eb91f26efa7b534eef4a535b491
wei0820 <jheng.wei0919@gmail.com>
2019-10-06 19:13:07 +0800

    commit

--- Sixtymaster/Sixtymaster/AppDelegate.swift
+++ Sixtymaster/Sixtymaster/AppDelegate.swift
@@ -8,34 +8,19 @@
 import UIKit
 import GoogleMobileAds
 import Firebase
-import GoogleSignIn
 import FacebookCore
 
 
 @UIApplicationMain
 
-class AppDelegate: UIResponder, UIApplicationDelegate , GIDSignInDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate  {
     var window: UIWindow?
-
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if let error = error {
-            print(error)
-            return
-        }
-     
-    }
-    
-    
-    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
      
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         FirebaseApp.configure()
-        
-        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
-             GIDSignIn.sharedInstance().delegate = self
         ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         
         return true
